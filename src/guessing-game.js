44eb13cbfd3a1f4379bf065686536d527afc9b68
Yurii <77541560+Nekhai@users.noreply.github.com>
2021-08-31 22:09:10 +0300

    feat: add Guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.value = null;
+     }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.ceil((this.max + this.min)/ 2);
+        return this.value;
     }
 
     lower() {
-
+        this.setRange(this.min, this.value);
     }
 
     greater() {
-
+        this.setRange(this.value, this.max);
     }
 }
 
